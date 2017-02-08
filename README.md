48ec6a7c28c0a639de23c14828cdf411969b1687
utsman <zeehastory@gmail.com>
2017-02-08 21:56:01 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# Spring Cloud Sample
+
+
+
+## Config-server
+
+Start the server:
+
+     ./gradlew :eureka:bootRun
+     
+     
+at browser:
+     
+     http://localhost:8888
+     
+     
+     
+     
+참고
+     
+     https://github.com/spring-cloud-samples/configserver
+     http://callistaenterprise.se/blogg/teknik/2015/04/10/building-microservices-with-spring-cloud-and-netflix-oss-part-1/
+     http://callistaenterprise.se/blogg/teknik/2015/05/20/blog-series-building-microservices/
---
+++ discovery-server/src/test/java/com/utsman/market/service/discovery/EurekaServerApplicationTest.java
@@ -0,0 +1,32 @@
+package com.utsman.market.service.discovery;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.embedded.EmbeddedWebApplicationContext;
+import org.springframework.boot.context.embedded.LocalServerPort;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.util.Map;
+
+import static org.junit.Assert.assertEquals;
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.DEFINED_PORT;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(webEnvironment = DEFINED_PORT)
+public class EurekaServerApplicationTest {
+    @Autowired
+    TestRestTemplate testRestTemplate;
+
+    @Test
+    public void catalogLoads() {
+        ResponseEntity<Map> entity =
+                testRestTemplate
+                        .getForEntity("/eureka/apps", Map.class);
+        assertEquals(HttpStatus.OK, entity.getStatusCode());
+    }
+}
