2db56dc81344bfa49ac9ba906134a349884a630d
utsman <zeehastory@gmail.com>
2017-02-16 15:28:16 +0900

    Refactor Test code

--- discovery-server/src/test/java/com/utsman/market/service/discovery/EurekaServerApplicationTest.java
+++ discovery-server/src/test/java/com/utsman/market/service/discovery/EurekaServerApplicationTest.java
@@ -3,8 +3,6 @@ package com.utsman.market.service.discovery;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.context.embedded.EmbeddedWebApplicationContext;
-import org.springframework.boot.context.embedded.LocalServerPort;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.HttpStatus;
@@ -14,10 +12,10 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import java.util.Map;
 
 import static org.junit.Assert.assertEquals;
-import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.DEFINED_PORT;
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringBootTest(webEnvironment = DEFINED_PORT)
+@SpringBootTest(webEnvironment = RANDOM_PORT)
 public class EurekaServerApplicationTest {
     @Autowired
     TestRestTemplate testRestTemplate;
